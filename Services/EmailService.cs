114c1cc42e9d3fb63dddd52107ef410e3b318a7d
SILAS-JOB <tiilas777@gmail.com>
2025-11-05 20:20:31 -0300

    feat: Email SendOrderAsync

---
+++ Services/EmailService.cs
@@ -0,0 +1,46 @@
+using ChicasEventos.Models;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+namespace TonhoLocacao.Services
+{
+
+    public interface IEmailService
+    {
+        Task SendOrderEmailAsync(string toEmail, string subject, string htmlBody);
+    }
+    public class EmailService : IEmailService
+    {
+        private readonly EmailSettings _emailSettings;
+
+        public EmailService(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+
+            _emailSettings = emailSettings?.Value ?? new EmailSettings();
+        }
+        public async Task SendOrderEmailAsync(string toEmail, string subject, string htmlBody)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(_emailSettings.SenderName, _emailSettings.SenderEmail));
+            message.To.Add(new MailboxAddress("", toEmail));
+            message.Subject = subject;
+
+            message.Body = new TextPart("html")
+            {
+                Text = htmlBody
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync(_emailSettings.SmtpServer, _emailSettings.Port, SecureSocketOptions.StartTls);
+                // await client.ConnectAsync(_emailSettings.SmtpServer, _emailSettings.Port, SecureSocketOptions.SslOnConnect);
+                await client.AuthenticateAsync(_emailSettings.Username, _emailSettings.Password);
+                await client.SendAsync(message);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
