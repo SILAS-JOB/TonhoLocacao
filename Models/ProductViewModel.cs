57ce8ebf092213f72de68a2640ab4d0eb34c241f
SILAS-JOB <tiilas777@gmail.com>
2025-11-05 20:20:05 -0300

    feat: product base model

---
+++ Models/ProductViewModel.cs
@@ -0,0 +1,13 @@
+namespace TonhoLocacao.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Value { get; set; }
+        
+
+
+    }
+}
