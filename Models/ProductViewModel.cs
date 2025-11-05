3f2350c80044729fbf9ee3963422b551f64daecb
SILAS-JOB <tiilas777@gmail.com>
2025-11-05 20:30:37 -0300

    feat: Product category

--- Models/ProductViewModel.cs
+++ Models/ProductViewModel.cs
@@ -3,10 +3,11 @@ namespace TonhoLocacao.Models
     public class Products
     {
         public int Id { get; set; }
+        public string Category { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int Value { get; set; }
-        
+
 
 
     }
