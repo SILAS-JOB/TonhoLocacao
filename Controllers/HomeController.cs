80c2eb760533581ff1449868361ceb6cd994be75
SILAS-JOB <tiilas777@gmail.com>
2025-11-08 09:09:53 -0300

    feat: functional pdfservice

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,20 @@
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Asn1.Iana;
 using TonhoLocacao.Models;
+using TonhoLocacao.Services;
 
 namespace TonhoLocacao.Controllers;
 
+
 public class HomeController : Controller
 {
+    private readonly PdfService _pdfService;
+    public HomeController(PdfService pdfService)
+    {
+        _pdfService = pdfService;
+    }
     public IActionResult Index()
     {
         return View();
@@ -15,6 +24,12 @@ public class HomeController : Controller
     {
         return View();
     }
+
+    public IActionResult StartPdfService()
+    {
+        byte[] pdfData = _pdfService.GeneratePdf();
+        return File(pdfData, "application/pdf", "Seu_Orçamento.pdf");
+    } 
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
