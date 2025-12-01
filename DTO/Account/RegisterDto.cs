d8ae9c599b6c4cc68277863ae15ec669d7776094
SILAS-JOB <tiilas777@gmail.com>
2025-11-30 23:02:49 -0300

    feat: model for user's login & register DTO

---
+++ DTO/Account/RegisterDto.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TonhoLocacao.DTO.Account;
+
+public class RegisterDto
+{
+    [Required(ErrorMessage = "Nome é obrigatório")]
+    [EmailAddress(ErrorMessage = "Email Inválido")]
+    public required string Name { get; set; }
+
+    [Required(ErrorMessage = "Senha é obrigatória")]
+    [MinLength(8, ErrorMessage = "A senha deve ter no mínimo 8 caracteres")]
+    public required string Password { get; set; }
+
+    [Compare("Password", ErrorMessage = "As senhas devem ser iguais")]
+    public required string ConfirmedPassword { get; set; }
+
+}
